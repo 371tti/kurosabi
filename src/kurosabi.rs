4ef1d8340cc499fb5a47ebce4ea941edc027684b
371tti <109227273+371tti@users.noreply.github.com>
2025-03-13 22:37:11 +0900

    Kurosabi構造体にコンテキストとルーターの依存性を追加し、HTTPメソッドのルーティングメソッドを改善

--- src/kurosabi.rs
+++ src/kurosabi.rs
@@ -1,82 +1,125 @@
-use std::io::Read;
-
-pub struct Kurosabi {
+use std::{io::Read, sync::Arc};
+    
+pub struct Kurosabi<C, Router> {
+    context: Arc<C>,
+    router: Arc<Router>
 }
 
 /// 初期化およびインスタンス操作を行うためのメソッドぐん
 /// 
-impl Kurosabi {
-    pub fn new() -> Kurosabi {
+impl<C, Router> Kurosabi<C, Router>
+where C: Context,
+    Router: GenRouter,
+{
+    pub fn new(context: Arc<C>, router: Arc<Router>) -> Kurosabi<C, Router> {
         Kurosabi {
+            context,
+            router
         }
     }
 }
 
+pub trait GenRouter {
+    fn regist(&mut self, method: Method, pattern: &str, index: usize) -> ();
+}
+
+pub trait Context {
+    fn get(&self, key: &str) -> Option<&str>;
+    fn set(&self, key: &str, value: &str) -> ();
+    fn remove(&self, key: &str) -> ();
+    fn clear(&self) -> ();
+    fn keys(&self) -> Vec<&str>;
+    fn values(&self) -> Vec<&str>;
+    fn iter(&self) -> impl Iterator<Item = (&str, &str)>;
+    fn len(&self) -> usize;
+}
+    
+
 /// レジストリ操作メソッドたち
 /// 
-impl Kurosabi {
+impl<C, Router> Kurosabi<C, Router> {
     /// httpのGETメソッドに対するルーティングを登録する
     pub fn get<F>(&mut self, pattern: &str, handler: F) -> () 
-    where F: AsyncFn(&mut Req) -> Res
+    where F: AsyncFn(&mut Req, Arc<C>) -> Res
     {
 
     }
 
     /// httpのPOSTメソッドに対するルーティングを登録する
     pub fn post<F>(&mut self, pattern: &str, handler: F) -> () 
-    where F: AsyncFn(&mut Req) -> Res
+    where F: AsyncFn(&mut Req, Arc<C>) -> Res
     {
 
     }
 
     /// httpのHEADメソッドに対するルーティングを登録する
     pub fn head<F>(&mut self, pattern: &str, handler: F) -> () 
-    where F: AsyncFn(&mut Req) -> Res
+    where F: AsyncFn(&mut Req, Arc<C>) -> Res
     {
 
     }
 
     /// httpのPUTメソッドに対するルーティングを登録する
     pub fn put<F>(&mut self, pattern: &str, handler: F) -> () 
-    where F: AsyncFn(&mut Req) -> Res
+    where F: AsyncFn(&mut Req, Arc<C>) -> Res
     {
 
     }
 
     /// httpのDELETEメソッドに対するルーティングを登録する
     pub fn delete<F>(&mut self, pattern: &str, handler: F) -> () 
-    where F: AsyncFn(&mut Req) -> Res
+    where F: AsyncFn(&mut Req, Arc<C>) -> Res
     {
 
     }
 
     /// httpのOPTIONSメソッドに対するルーティングを登録する
     pub fn options<F>(&mut self, pattern: &str, handler: F) -> () 
-    where F: AsyncFn(&mut Req) -> Res
+    where F: AsyncFn(&mut Req, Arc<C>) -> Res
     {
 
     }
 
     /// httpのTRACEメソッドに対するルーティングを登録する
     pub fn trace<F>(&mut self, pattern: &str, handler: F) -> () 
-    where F: AsyncFn(&mut Req) -> Res
+    where F: AsyncFn(&mut Req, Arc<C>) -> Res
     {
 
     }
 
     /// httpのCONNECTメソッドに対するルーティングを登録する
     pub fn connect<F>(&mut self, pattern: &str, handler: F) -> () 
-    where F: AsyncFn(&mut Req) -> Res
+    where F: AsyncFn(&mut Req, Arc<C>) -> Res
     {
 
     }
 
     /// httpのPATCHメソッドに対するルーティングを登録する
     pub fn patch<F>(&mut self, pattern: &str, handler: F) -> () 
-    where F: AsyncFn(&mut Req) -> Res
+    where F: AsyncFn(&mut Req, Arc<C>) -> Res
     {
 
     }
+
+    pub fn some_method<F>(&mut self, pattern: &str, handler: F) -> () 
+    where F: AsyncFn(&mut Req, Arc<C>) -> Res
+    {
+
+    }
+
+    pub fn before<F>(&mut self, pattern: &str, handler: F) -> () 
+    where F: AsyncFn(&mut Req, Arc<C>) -> Res
+    {
+
+    }
+
+    pub fn after<F>(&mut self, pattern: &str, handler: F) -> () 
+    where F: AsyncFn(&mut Req, Arc<C>) -> Res
+    {
+
+    }
+
+
 }
 
 pub struct Req {
@@ -89,34 +132,64 @@ pub struct Path {
     /// パスの文字列(完全)を保持
     /// 遅延処理をする
     pub path: String,
-    pub segments: Option<Vec<String>>,
-    pub query: Option<Vec<(String, String)>>,
-
+    pub segments: Segments,
+    pub query: Query,
 }
 
 impl Path {
     pub fn new() -> Path {
         Path {
-    
             path: String::new(),
-            segments: None,
-            query: None,
+            segments: Segments::new(),
+            query: Query::new(),
         }
     }
 
     pub fn dec_segment(&mut self) {
-        self.segments = Some(self.path.split('/').filter(|s| !s.is_empty()).map(|s| s.to_string()).collect());
+        self.segments.segments = self.path.split('/')
+            .filter(|s| !s.is_empty())
+            .map(String::from)
+            .collect();
     }
 
     pub fn dec_query(&mut self) {
-        self.query = Some(self.path.split('?').nth(1).unwrap_or("").split('&').filter_map(|s| {
-            let mut iter = s.split('=');
-            if let (Some(key), Some(value)) = (iter.next(), iter.next()) {
-                Some((key.to_string(), value.to_string()))
-            } else {
-                None
-            }
-        }).collect());
+        self.query.query = self.path.split('?')
+            .nth(1)
+            .unwrap_or("")
+            .split('&')
+            .filter_map(|s| {
+                let mut iter = s.splitn(2, '=');
+                if let (Some(key), Some(value)) = (iter.next(), iter.next()) {
+                    Some((key.to_string(), value.to_string()))
+                } else {
+                    None
+                }
+            })
+            .collect();
+    }
+}
+
+pub struct Segments {
+    pub segments: Vec<String>,
+}
+
+impl Segments {
+    pub fn new() -> Segments {
+        Segments {
+            segments: Vec::new(),
+        }
+    }
+}
+
+pub struct Query {
+    pub query: Vec<(String, String)>,
+}
+
+impl Query {
+    pub fn new() -> Query {
+        Query {
+            query: Vec::new(),
+        }
     }
 }
 
