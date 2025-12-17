6e650981c0de7f42c72050742b37686275d53f57
371tti <sannnanaitti@gmail.com>
2025-12-17 13:39:27 +0900

    Remove unused HTTP-related modules and worker implementation
    - Deleted the `worker.rs` file which contained the worker trait and its default implementation.
    - Removed the `header.rs`, `method.rs`, `reader.rs`, `status.rs`, and `mod.rs` files from the utils directory, which included HTTP header handling, HTTP methods enumeration, and status code definitions.
    - Added a new `mod.rs` file in the `http` directory to prepare for future HTTP-related definitions and functionalities.

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,2 @@
-pub mod kurosabi;
-pub mod context;
-pub mod request;
-pub mod response;
-pub mod router;
-pub mod utils;
+pub mod http;
 pub mod server;
-pub mod error;
-pub mod api;
-// pub mod middleware;
-
-pub use crate::kurosabi::Kurosabi as Kurosabi;
-pub use html_format::html_format as html_format;
-pub use tokio::main as tokio_main;
--- src/utils/mod.rs
+++
@@ -1,3 +0,0 @@
-pub mod header;
-pub mod status;
-pub mod method;
