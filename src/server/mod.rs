4c6a8cdd04799e4707d2f2b8dec8781dbebfdc7f
371tti <sannnanaitti@gmail.com>
2025-12-17 13:43:58 +0900

    feat: TCPリスナーと接続の構造体を追加

--- src/server/mod.rs
+++ src/server/mod.rs
@@ -3,3 +3,19 @@
 /// kurosabiはその柔軟性を維持するためにTCPレイヤーで可能な限り開発者が直接操作可能なメソッドを提供することを目指す
 /// TCPレイヤーを最も下とする これはOS上で動くことを想定しているから
 /// 
+/// 
+
+
+pub struct TCPListener {
+    pub listener: compio::net::TcpListener,
+}
+
+pub struct TCPConnection {
+    pub stream: compio::net::TcpStream,
+}
+
+pub struct ConnectionQueue {
+    
+}
+
+pub struct HTTP {}
